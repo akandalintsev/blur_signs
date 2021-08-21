74cd79ecceb8fd66d3ac28fbeb468947912e2a23
Aleksey Kandalintsev <aleksey.kandalintsev@gmail.com>
2021-08-21 18:36:31 +0400

    detect coords

--- detect_coordinates/main.py
+++ detect_coordinates/main.py
@@ -1,59 +1,54 @@
 import os
-
-# Specify device
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
-
-# Import all necessary libraries.
 import numpy as np
 import sys
 import cv2
-
-# NomeroffNet path
-NOMEROFF_NET_DIR = os.path.abspath('../')
-
-sys.path.append(NOMEROFF_NET_DIR)
-
 # Import license plate recognition tools.
 from NomeroffNet.YoloV5Detector import Detector
-
-detector = Detector()
-detector.load()
-
 # from NomeroffNet.OptionsDetector import OptionsDetector
 from NomeroffNet.TextDetector import TextDetector
 
 from NomeroffNet import TextDetector
 from NomeroffNet import textPostprocessing
 
-# load models
-# optionsDetector = OptionsDetector()
-# optionsDetector.load("latest")
+# Specify device
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
 
+# NomeroffNet path
+NOMEROFF_NET_DIR = os.path.abspath('../')
+
+sys.path.append(NOMEROFF_NET_DIR)
+
+detector = Detector()
+detector.load()
+
+imgs = [
+    '21_5_2014_7_42_58_269.bmp',
+    '21_5_2014_19_16_58_117.bmp'
+]
+base_dir = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'in',
+    'PhotoBaseFull')
+img_paths = []
+for p in imgs:
+    img_paths.append(os.path.join(base_dir, p))
+
+# load models
 textDetector = TextDetector.get_static_module("eu")
 textDetector.load("latest")
 
 # Detect numberplate
-img_path = 'images/example2.jpeg'
-img = cv2.imread(img_path)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+xx = []
+yy = []
 
-targetBoxes = detector.detect_bbox(img)
+for img_path in img_paths:
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    targetBoxes = detector.detect_bbox(img)
 
-zones = []
-regionNames = []
-for targetBox in targetBoxes:
-    x = int(min(targetBox[0], targetBox[2]))
-    w = int(abs(targetBox[2] - targetBox[0]))
-    y = int(min(targetBox[1], targetBox[3]))
-    h = int(abs(targetBox[3] - targetBox[1]))
+    for targetBox in targetBoxes:
+        xx.append(int(min(targetBox[0], targetBox[2])))
+        yy.append(int(min(targetBox[1], targetBox[3])))
 
-    image_part = img[y:y + h, x:x + w]
-    zones.append(image_part)
-    regionNames.append('eu')
-
-# find text with postprocessing by standart
-textArr = textDetector.predict(zones)
-textArr = textPostprocessing(textArr, regionNames)
-print(textArr)
-# ['RP70012', 'JJF509']
+print(xx)
+print(yy)
